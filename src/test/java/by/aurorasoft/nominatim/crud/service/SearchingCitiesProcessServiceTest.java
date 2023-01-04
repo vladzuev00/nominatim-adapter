4181912a8dcf351a93183e5340785ce6ab5a0cf1
vladzuev <vladzuev.00@mail.ru>
2023-01-04 04:47:34 -0600

    add additional tests for SearchingCitiesProcessService

--- src/test/java/by/aurorasoft/nominatim/crud/service/SearchingCitiesProcessServiceTest.java
+++ src/test/java/by/aurorasoft/nominatim/crud/service/SearchingCitiesProcessServiceTest.java
@@ -9,9 +9,13 @@ import org.locationtech.jts.geom.GeometryFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.util.List;
+
 import static by.aurorasoft.nominatim.crud.model.entity.SearchingCitiesProcessEntity.Status.ERROR;
 import static by.aurorasoft.nominatim.crud.model.entity.SearchingCitiesProcessEntity.Status.HANDLING;
+import static java.util.stream.Collectors.toList;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public final class SearchingCitiesProcessServiceTest extends AbstractContextTest {
 
@@ -79,5 +83,30 @@ public final class SearchingCitiesProcessServiceTest extends AbstractContextTest
                 .status(HANDLING)
                 .build();
         assertEquals(expected, actual);
+    }
+
+    @Test
+    @Sql(statements = "INSERT INTO searching_cities_process "
+            + "(id, bounds, search_step, total_points, handled_points, status) "
+            + "VALUES(255, ST_GeomFromText('POLYGON((1 2, 3 4, 5 6, 6 7, 1 2))'), 0.01, 10000, 1000, 'HANDLING')")
+    @Sql(statements = "INSERT INTO searching_cities_process "
+            + "(id, bounds, search_step, total_points, handled_points, status) "
+            + "VALUES(256, ST_GeomFromText('POLYGON((1 2, 3 4, 5 6, 6 7, 1 2))'), 0.01, 10000, 1000, 'HANDLING')")
+    @Sql(statements = "INSERT INTO searching_cities_process "
+            + "(id, bounds, search_step, total_points, handled_points, status) "
+            + "VALUES(257, ST_GeomFromText('POLYGON((1 2, 3 4, 5 6, 6 7, 1 2))'), 0.01, 10000, 1000, 'ERROR')")
+    public void processesShouldBeFoundByStatus() {
+        final List<SearchingCitiesProcess> foundProcesses = this.service.findByStatus(HANDLING);
+        final List<Long> actualIds = foundProcesses.stream()
+                .map(SearchingCitiesProcess::getId)
+                .collect(toList());
+        final List<Long> expectedIds = List.of(255L, 256L);
+        assertEquals(expectedIds, actualIds);
+    }
+
+    @Test
+    public void processesShouldNotBeFoundByStatus() {
+        final List<SearchingCitiesProcess> foundProcesses = this.service.findByStatus(HANDLING);
+        assertTrue(foundProcesses.isEmpty());
     }
 }
