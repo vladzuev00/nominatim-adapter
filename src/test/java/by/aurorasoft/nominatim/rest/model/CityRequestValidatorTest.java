f92d991ecd5d311d15e9dd462e56f544368f4bd0
vladzuev <vladzuev.00@mail.ru>
2023-01-05 05:50:00 -0600

    add test for CityRequestValidator

---
+++ src/test/java/by/aurorasoft/nominatim/rest/model/CityRequestValidatorTest.java
@@ -0,0 +1,43 @@
+package by.aurorasoft.nominatim.rest.model;
+
+import by.aurorasoft.nominatim.rest.controller.exception.ConstraintException;
+import by.aurorasoft.nominatim.rest.validator.CityRequestValidator;
+import org.junit.Test;
+import org.springframework.validation.Errors;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public final class CityRequestValidatorTest {
+
+    private final CityRequestValidator validator;
+
+    public CityRequestValidatorTest() {
+        this.validator = new CityRequestValidator();
+    }
+
+    @Test
+    public void validationShouldBeSuccessful() {
+        final Errors givenErrors = mock(Errors.class);
+        when(givenErrors.hasErrors()).thenReturn(false);
+        this.validator.validate(givenErrors);
+    }
+
+    @Test
+    public void validationShouldBeFailed() {
+        final Errors givenErrors = mock(Errors.class);
+        when(givenErrors.hasErrors()).thenReturn(true);
+
+        ConstraintException expectedException = null;
+        try {
+            this.validator.validate(givenErrors);
+        } catch (final ConstraintException exception) {
+            expectedException = exception;
+        }
+
+        assertNotNull(expectedException);
+        assertSame(expectedException.findErrors().orElseThrow(), givenErrors);
+    }
+}
