c370fbd81f9fb8ada046acaca2ed3c35d056ed32
vladzuev <vladzuev.00@mail.ru>
2023-01-05 09:56:21 -0600

    add unique constraint of geometry in table 'city'

---
+++ src/test/java/by/aurorasoft/nominatim/service/SearchCityServiceTest.java
@@ -0,0 +1,11 @@
+package by.aurorasoft.nominatim.service;
+
+import org.junit.Test;
+
+public class SearchCityServiceTest {
+
+    @Test
+    public void a() {
+        throw new RuntimeException();
+    }
+}
