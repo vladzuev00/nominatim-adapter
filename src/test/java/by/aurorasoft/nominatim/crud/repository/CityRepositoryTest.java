e4760746688e6b8e7279a686da221f577c50e650
Vlad Zuev <vladzuev.00@mail.ru>
2023-01-06 14:28:29 +0300

    add tests for CityRepository::isExistByGeometry

--- src/test/java/by/aurorasoft/nominatim/crud/repository/CityRepositoryTest.java
+++ src/test/java/by/aurorasoft/nominatim/crud/repository/CityRepositoryTest.java
@@ -5,28 +5,28 @@ import by.aurorasoft.nominatim.crud.model.entity.CityEntity;
 import org.junit.Test;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.CoordinateXY;
+import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
 
 import static by.aurorasoft.nominatim.crud.model.entity.CityEntity.Type.CAPITAL;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertSame;
+import static org.junit.Assert.*;
 
 public final class CityRepositoryTest extends AbstractContextTest {
 
     @Autowired
-    private CityRepository cityRepository;
+    private CityRepository repository;
 
     @Autowired
     private GeometryFactory geometryFactory;
 
     @Test
     @Sql(statements = "INSERT INTO city(id, name, geometry, type) VALUES(255, 'Minsk', "
-            + "ST_GeomFromText('POLYGON((1 2, 3 4, 5 6, 6 7, 1 2))'), 'CAPITAL')")
+            + "ST_GeomFromText('POLYGON((1 2, 3 4, 5 6, 6 7, 1 2))', 4326), 'CAPITAL')")
     public void cityShouldBeFoundById() {
         super.startQueryCount();
-        final CityEntity actual = this.cityRepository.findById(255L).orElseThrow();
+        final CityEntity actual = this.repository.findById(255L).orElseThrow();
         super.checkQueryCount(1);
 
         final Coordinate[] expectedCoordinates = new Coordinate[]{
@@ -61,8 +61,54 @@ public final class CityRepositoryTest extends AbstractContextTest {
                 .build();
 
         super.startQueryCount();
-        this.cityRepository.save(givenCity);
+        this.repository.save(givenCity);
         super.checkQueryCount(1);
+    }
+
+    @Test
+    @Sql(statements = "INSERT INTO city(id, name, geometry, type) VALUES(255, 'Minsk', "
+            + "ST_GeomFromText('POLYGON((1 2, 3 4, 5 6, 6 7, 1 2))', 4326), 'CAPITAL')")
+    @Sql(statements = "INSERT INTO city(id, name, geometry, type) VALUES(256, 'Minsk', "
+            + "ST_GeomFromText('POLYGON((1 3, 3 4, 5 6, 6 7, 1 3))', 4326), 'CAPITAL')")
+    @Sql(statements = "INSERT INTO city(id, name, geometry, type) VALUES(257, 'Minsk', "
+            + "ST_GeomFromText('POLYGON((1 4, 3 4, 5 6, 6 7, 1 4))', 4326), 'CAPITAL')")
+    public void cityWithGivenGeometryShouldExist() {
+        final Coordinate[] givenCoordinates = new Coordinate[]{
+                new CoordinateXY(1, 2),
+                new CoordinateXY(3, 4),
+                new CoordinateXY(5, 6),
+                new CoordinateXY(6, 7),
+                new CoordinateXY(1, 2)
+        };
+        final Geometry givenGeometry = this.geometryFactory.createPolygon(givenCoordinates);
+
+        super.startQueryCount();
+        final boolean exists = this.repository.isExistByGeometry(givenGeometry);
+        super.checkQueryCount(1);
+        assertTrue(exists);
+    }
+
+    @Test
+    @Sql(statements = "INSERT INTO city(id, name, geometry, type) VALUES(255, 'Minsk', "
+            + "ST_GeomFromText('POLYGON((1 5, 3 4, 5 6, 6 7, 1 5))', 4326), 'CAPITAL')")
+    @Sql(statements = "INSERT INTO city(id, name, geometry, type) VALUES(256, 'Minsk', "
+            + "ST_GeomFromText('POLYGON((1 3, 3 4, 5 6, 6 7, 1 3))', 4326), 'CAPITAL')")
+    @Sql(statements = "INSERT INTO city(id, name, geometry, type) VALUES(257, 'Minsk', "
+            + "ST_GeomFromText('POLYGON((1 4, 3 4, 5 6, 6 7, 1 4))', 4326), 'CAPITAL')")
+    public void cityWithGivenGeometryShouldNotExist() {
+        final Coordinate[] givenCoordinates = new Coordinate[]{
+                new CoordinateXY(1, 2),
+                new CoordinateXY(3, 4),
+                new CoordinateXY(5, 6),
+                new CoordinateXY(6, 7),
+                new CoordinateXY(1, 2)
+        };
+        final Geometry givenGeometry = this.geometryFactory.createPolygon(givenCoordinates);
+
+        super.startQueryCount();
+        final boolean exists = this.repository.isExistByGeometry(givenGeometry);
+        super.checkQueryCount(1);
+        assertFalse(exists);
     }
 
     private static void checkEquals(CityEntity expected, CityEntity actual) {
