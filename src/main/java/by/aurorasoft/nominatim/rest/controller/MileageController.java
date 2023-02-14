5d5c3db89e6e5e381524775cab72dc3e485e764e
Vlad Zuev <vladzuev.00@mail.ru>
2023-02-14 16:28:27 +0300

    finish mileage controller

--- src/main/java/by/aurorasoft/nominatim/rest/controller/MileageController.java
+++ src/main/java/by/aurorasoft/nominatim/rest/controller/MileageController.java
@@ -2,15 +2,25 @@ package by.aurorasoft.nominatim.rest.controller;
 
 import by.aurorasoft.nominatim.rest.model.MileageRequest;
 import by.aurorasoft.nominatim.rest.model.MileageResponse;
+import by.aurorasoft.nominatim.service.mileage.MileageService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 @RequestMapping("/mileage")
+@Validated
+@RequiredArgsConstructor
 public class MileageController {
+    private final MileageService mileageService;
 
     @PostMapping
-    public ResponseEntity<MileageResponse> findMileage(@RequestBody MileageRequest mileageRequest) {
-        return null;
+    public ResponseEntity<MileageResponse> findMileage(@Valid @RequestBody MileageRequest mileageRequest) {
+        return ok(this.mileageService.findMileage(mileageRequest));
     }
 }
