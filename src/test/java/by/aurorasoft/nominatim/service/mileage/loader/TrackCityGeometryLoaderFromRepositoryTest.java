343482b0e2bc6b3f625185c0a9d0af7242a5e7c6
Vlad Zuev <vladzuev.00@mail.ru>
2024-04-16 12:21:44 +0300

    add TrackCityGeometryLoaderFromRepositoryTest

---
+++ src/test/java/by/aurorasoft/nominatim/service/mileage/loader/TrackCityGeometryLoaderFromRepositoryTest.java
@@ -0,0 +1,43 @@
+package by.aurorasoft.nominatim.service.mileage.loader;
+
+import by.aurorasoft.nominatim.crud.service.CityService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.locationtech.jts.geom.LineString;
+import org.locationtech.jts.geom.prep.PreparedGeometry;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertSame;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public final class TrackCityGeometryLoaderFromRepositoryTest {
+
+    @Mock
+    private CityService mockedCityService;
+
+    private TrackCityGeometryLoaderFromRepository loader;
+
+    @Before
+    public void initializeLoader() {
+        loader = new TrackCityGeometryLoaderFromRepository(null, null, mockedCityService);
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void geometriesShouldBeLoaded() {
+        final LineString givenLine = mock(LineString.class);
+
+        final List<PreparedGeometry> givenGeometries = mock(List.class);
+        when(mockedCityService.findIntersectedPreparedGeometries(same(givenLine))).thenReturn(givenGeometries);
+
+        final List<PreparedGeometry> actual = loader.load(givenLine);
+        assertSame(givenGeometries, actual);
+    }
+}
