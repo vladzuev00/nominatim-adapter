0d16b074a77499da1dcee12b1be846769fc8b1a4
Vlad Zuev <vladzuev.00@mail.ru>
2024-04-16 17:19:29 +0300

    refactor

--- src/test/java/by/aurorasoft/nominatim/it/MileageCalculationWithCachedCityGeometriesIT.java
+++ src/test/java/by/aurorasoft/nominatim/it/MileageCalculationWithCachedCityGeometriesIT.java
@@ -4,6 +4,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
+@SuppressWarnings("SpringBootApplicationProperties")
 @SpringBootTest(webEnvironment = RANDOM_PORT, properties = "mileage-calc.load-city-geometries-on-start-app=true")
 public final class MileageCalculationWithCachedCityGeometriesIT extends MileageCalculationIT {
 
