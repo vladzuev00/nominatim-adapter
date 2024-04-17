329b41dcd1b66d9e9734c4733e7a5991933286d9
Vlad Zuev <vladzuev.00@mail.ru>
2024-04-17 16:53:16 +0300

    add gist index for bounding boxes

--- src/main/resources/db/changelog/scripts/1.0.sql
+++ src/main/resources/db/changelog/scripts/1.0.sql
@@ -13,35 +13,5 @@ CREATE TABLE city
 
 ALTER SEQUENCE city_id_seq INCREMENT 50;
 
-CREATE TYPE searching_cities_process_type AS ENUM('HANDLING', 'SUCCESS', 'ERROR');
-
-CREATE TABLE searching_cities_process
-(
-    id             SERIAL PRIMARY KEY,
-    status         searching_cities_process_type NOT NULL,
-    search_step    DECIMAL                       NOT NULL,
-    total_points   BIGINT                        NOT NULL,
-    handled_points BIGINT                        NOT NULL,
-    bounds         GEOMETRY                      NOT NULL,
-    updated_time   TIMESTAMP                     NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    created_time   TIMESTAMP                     NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE
-OR REPLACE FUNCTION on_update_searching_cities_process() RETURNS TRIGGER AS
-'
-    BEGIN
-        NEW.updated_time = CURRENT_TIMESTAMP;
-        RETURN NEW;
-    END;
-' LANGUAGE plpgsql;
-
-CREATE TRIGGER tr_on_update_searching_cities_process
-    BEFORE UPDATE
-    ON searching_cities_process
-    FOR EACH ROW
-    EXECUTE PROCEDURE on_update_searching_cities_process();
-
-CREATE INDEX ON city using GIST(geometry);
-CREATE INDEX ON city using GIST(bounding_box);
+CREATE INDEX city_bounding_box_index ON city USING GIST(bounding_box);
 
