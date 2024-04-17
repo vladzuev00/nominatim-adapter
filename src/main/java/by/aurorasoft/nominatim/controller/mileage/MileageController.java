89e9e6fe39cb209e7bbc4c6808415d8f994ba098
Vlad Zuev <vladzuev.00@mail.ru>
2024-04-17 10:27:01 +0300

    refactor MileageController

--- src/main/java/by/aurorasoft/nominatim/controller/mileage/MileageController.java
+++ src/main/java/by/aurorasoft/nominatim/controller/mileage/MileageController.java
@@ -1,12 +1,12 @@
 package by.aurorasoft.nominatim.controller.mileage;
 
-import by.aurorasoft.nominatim.model.Track;
-import by.aurorasoft.nominatim.model.TrackPoint;
-import by.aurorasoft.nominatim.model.Mileage;
+import by.aurorasoft.nominatim.controller.mileage.factory.DistanceCalculatorSettingsFactory;
+import by.aurorasoft.nominatim.controller.mileage.factory.TrackFactory;
 import by.aurorasoft.nominatim.controller.mileage.model.MileageRequest;
-import by.aurorasoft.nominatim.controller.mileage.model.MileageRequest.RequestTrackPoint;
+import by.aurorasoft.nominatim.model.Mileage;
+import by.aurorasoft.nominatim.model.Track;
 import by.aurorasoft.nominatim.service.mileage.MileageCalculatingService;
-import by.nhorushko.distancecalculator.DistanceCalculatorSettingsImpl;
+import by.nhorushko.distancecalculator.DistanceCalculatorSettings;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.List;
 
 import static org.springframework.http.ResponseEntity.ok;
 
@@ -25,27 +24,15 @@ import static org.springframework.http.ResponseEntity.ok;
 @Validated
 @RequiredArgsConstructor
 public class MileageController {
+    private final TrackFactory trackFactory;
+    private final DistanceCalculatorSettingsFactory distanceCalculatorSettingsFactory;
     private final MileageCalculatingService calculatingService;
 
     @PostMapping
-    public ResponseEntity<Mileage> findMileage(@Valid @RequestBody final MileageRequest mileageRequest) {
-        final Mileage mileage = calculatingService.calculate(createTrack(mileageRequest), new DistanceCalculatorSettingsImpl(mileageRequest.getMinDetectionSpeed(), mileageRequest.getMaxMessageTimeout()));
+    public ResponseEntity<Mileage> findMileage(@Valid @RequestBody final MileageRequest request) {
+        final Track track = trackFactory.create(request);
+        final DistanceCalculatorSettings distanceCalculatorSettings = distanceCalculatorSettingsFactory.create(request);
+        final Mileage mileage = calculatingService.calculate(track, distanceCalculatorSettings);
         return ok(mileage);
-    }
-
-    private static Track createTrack(final MileageRequest request) {
-        final List<TrackPoint> trackPoints = request.getTrackPoints().stream().map(MileageController::mapToTrackPoint).toList();
-        return new Track(trackPoints);
-    }
-
-    private static TrackPoint mapToTrackPoint(final RequestTrackPoint request) {
-        return new TrackPoint(
-                request.getDatetime(),
-                request.getLatitude(),
-                request.getLongitude(),
-                request.getAltitude(),
-                request.getSpeed(),
-                request.getValid()
-        );
     }
 }
