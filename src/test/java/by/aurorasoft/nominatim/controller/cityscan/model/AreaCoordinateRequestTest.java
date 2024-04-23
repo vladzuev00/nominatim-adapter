a9594141d151bd7504155e7eb07e7c2afa1ec8cb
Vlad Zuev <vladzuev.00@mail.ru>
2024-04-23 10:32:38 +0300

    add AreaCoordinateRequestTest

--- src/test/java/by/aurorasoft/nominatim/controller/cityscan/model/AreaCoordinateRequestTest.java
+++ src/test/java/by/aurorasoft/nominatim/controller/cityscan/model/AreaCoordinateRequestTest.java
@@ -9,6 +9,7 @@ import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
 import java.util.Set;
 
+import static by.aurorasoft.nominatim.util.ConstraintViolationUtil.findFirstMessage;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
@@ -59,5 +60,129 @@ public final class AreaCoordinateRequestTest extends AbstractJunitSpringBootTest
 
         final Set<ConstraintViolation<AreaCoordinateRequest>> violations = validator.validate(givenRequest);
         assertTrue(violations.isEmpty());
+    }
+
+    @Test
+    public void requestShouldNotBeValidBecauseOfMinLatitudeNotDefined() {
+        final AreaCoordinateRequest givenRequest = AreaCoordinateRequest.builder()
+                .minLongitude(46.)
+                .maxLatitude(47.)
+                .maxLongitude(48.)
+                .build();
+
+        final Set<ConstraintViolation<AreaCoordinateRequest>> violations = validator.validate(givenRequest);
+        assertEquals(1, violations.size());
+        assertEquals("Invalid latitude", findFirstMessage(violations));
+    }
+
+    @Test
+    public void requestShouldNotBeValidBecauseOfMinLatitudeIsLessThanMinimalAllowable() {
+        final AreaCoordinateRequest givenRequest = new AreaCoordinateRequest(-90.1, 46., 47., 48.);
+
+        final Set<ConstraintViolation<AreaCoordinateRequest>> violations = validator.validate(givenRequest);
+        assertEquals(1, violations.size());
+        assertEquals("Invalid latitude", findFirstMessage(violations));
+    }
+
+    @Test
+    public void requestShouldNotBeValidBecauseOfMinLatitudeIsMoreThanMaximalAllowable() {
+        final AreaCoordinateRequest givenRequest = new AreaCoordinateRequest(90.1, 46., 47., 48.);
+
+        final Set<ConstraintViolation<AreaCoordinateRequest>> violations = validator.validate(givenRequest);
+        assertEquals(1, violations.size());
+        assertEquals("Invalid latitude", findFirstMessage(violations));
+    }
+
+    @Test
+    public void requestShouldNotBeValidBecauseOfMinLongitudeNotDefined() {
+        final AreaCoordinateRequest givenRequest = AreaCoordinateRequest.builder()
+                .minLatitude(46.)
+                .maxLatitude(47.)
+                .maxLongitude(48.)
+                .build();
+
+        final Set<ConstraintViolation<AreaCoordinateRequest>> violations = validator.validate(givenRequest);
+        assertEquals(1, violations.size());
+        assertEquals("Invalid longitude", findFirstMessage(violations));
+    }
+
+    @Test
+    public void requestShouldNotBeValidBecauseOfMinLongitudeIsLessThanMinimalAllowable() {
+        final AreaCoordinateRequest givenRequest = new AreaCoordinateRequest(45., -180.1, 47., 48.);
+
+        final Set<ConstraintViolation<AreaCoordinateRequest>> violations = validator.validate(givenRequest);
+        assertEquals(1, violations.size());
+        assertEquals("Invalid longitude", findFirstMessage(violations));
+    }
+
+    @Test
+    public void requestShouldNotBeValidBecauseOfMinLongitudeIsMoreThanMaximalAllowable() {
+        final AreaCoordinateRequest givenRequest = new AreaCoordinateRequest(45., 180.1, 47., 48.);
+
+        final Set<ConstraintViolation<AreaCoordinateRequest>> violations = validator.validate(givenRequest);
+        assertEquals(1, violations.size());
+        assertEquals("Invalid longitude", findFirstMessage(violations));
+    }
+
+    @Test
+    public void requestShouldNotBeValidBecauseOfMaxLatitudeNotDefined() {
+        final AreaCoordinateRequest givenRequest = AreaCoordinateRequest.builder()
+                .minLatitude(45.)
+                .minLongitude(46.)
+                .maxLongitude(48.)
+                .build();
+
+        final Set<ConstraintViolation<AreaCoordinateRequest>> violations = validator.validate(givenRequest);
+        assertEquals(1, violations.size());
+        assertEquals("Invalid latitude", findFirstMessage(violations));
+    }
+
+    @Test
+    public void requestShouldNotBeValidBecauseOfMaxLatitudeIsLessThanMinimalAllowable() {
+        final AreaCoordinateRequest givenRequest = new AreaCoordinateRequest(45., 46., -90.1, 48.);
+
+        final Set<ConstraintViolation<AreaCoordinateRequest>> violations = validator.validate(givenRequest);
+        assertEquals(1, violations.size());
+        assertEquals("Invalid latitude", findFirstMessage(violations));
+    }
+
+    @Test
+    public void requestShouldNotBeValidBecauseOfMaxLatitudeIsMoreThanMaximalAllowable() {
+        final AreaCoordinateRequest givenRequest = new AreaCoordinateRequest(45., 46., 90.1, 48.);
+
+        final Set<ConstraintViolation<AreaCoordinateRequest>> violations = validator.validate(givenRequest);
+        assertEquals(1, violations.size());
+        assertEquals("Invalid latitude", findFirstMessage(violations));
+    }
+
+    @Test
+    public void requestShouldNotBeValidBecauseOfMaxLongitudeNotDefined() {
+        final AreaCoordinateRequest givenRequest = AreaCoordinateRequest.builder()
+                .minLatitude(45.)
+                .minLongitude(46.)
+                .maxLatitude(48.)
+                .build();
+
+        final Set<ConstraintViolation<AreaCoordinateRequest>> violations = validator.validate(givenRequest);
+        assertEquals(1, violations.size());
+        assertEquals("Invalid longitude", findFirstMessage(violations));
+    }
+
+    @Test
+    public void requestShouldNotBeValidBecauseOfMaxLongitudeIsLessThanMinimalAllowable() {
+        final AreaCoordinateRequest givenRequest = new AreaCoordinateRequest(45., 46., 47., -180.1);
+
+        final Set<ConstraintViolation<AreaCoordinateRequest>> violations = validator.validate(givenRequest);
+        assertEquals(1, violations.size());
+        assertEquals("Invalid longitude", findFirstMessage(violations));
+    }
+
+    @Test
+    public void requestShouldNotBeValidBecauseOfMaxLongitudeIsMoreThanMaximalAllowable() {
+        final AreaCoordinateRequest givenRequest = new AreaCoordinateRequest(45., 46., 47., 180.1);
+
+        final Set<ConstraintViolation<AreaCoordinateRequest>> violations = validator.validate(givenRequest);
+        assertEquals(1, violations.size());
+        assertEquals("Invalid longitude", findFirstMessage(violations));
     }
 }
