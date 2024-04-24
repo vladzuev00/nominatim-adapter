afa42d9cdb7f4a2e8537431fc2a80dbd7586d1d3
Vlad Zuev <vladzuev.00@mail.ru>
2024-04-24 12:16:03 +0300

    refactor MileagePercentageTest

--- src/test/java/by/aurorasoft/nominatim/model/MileagePercentageTest.java
+++ src/test/java/by/aurorasoft/nominatim/model/MileagePercentageTest.java
@@ -8,36 +8,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import static org.junit.Assert.assertEquals;
 import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
 
-public final class MileageTest extends AbstractJunitSpringBootTest {
+public final class MileagePercentageTest extends AbstractJunitSpringBootTest {
 
     @Autowired
     private ObjectMapper objectMapper;
 
     @Test
-    public void mileageShouldBeConvertedToJson()
+    public void mileagePercentageShouldBeConvertedToJson()
             throws Exception {
-        final MileagePercentage givenMileage = new MileagePercentage(5.5, 6.6);
+        final MileagePercentage givenMileagePercentage = new MileagePercentage(0.7, 0.3);
 
-        final String actual = objectMapper.writeValueAsString(givenMileage);
+        final String actual = objectMapper.writeValueAsString(givenMileagePercentage);
         final String expected = """
                 {
-                  "urban": 5.5,
-                  "country": 6.6
+                  "urban": 0.7,
+                  "country": 0.3
                 }""";
         assertEquals(expected, actual, true);
     }
 
     @Test
-    public void jsonShouldBeConvertedToMileage()
+    public void jsonShouldBeConvertedToMileagePercentage()
             throws Exception {
         final String givenJson = """
                 {
-                  "urban": 5.5,
-                  "country": 6.6
+                  "urban": 0.7,
+                  "country": 0.3
                 }""";
 
         final MileagePercentage actual = objectMapper.readValue(givenJson, MileagePercentage.class);
-        final MileagePercentage expected = new MileagePercentage(5.5, 6.6);
+        final MileagePercentage expected = new MileagePercentage(0.7, 0.3);
         assertEquals(expected, actual);
     }
 }
