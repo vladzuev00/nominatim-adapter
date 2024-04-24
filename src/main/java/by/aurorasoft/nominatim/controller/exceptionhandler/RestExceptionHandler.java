405c6f473799e2bc5f9c7463f5d0e83c96343fdd
Vlad Zuev <vladzuev.00@mail.ru>
2024-04-24 11:31:11 +0300

    refactor RestExceptionHandler and add RestExceptionHandlerTest

--- src/main/java/by/aurorasoft/nominatim/controller/exceptionhandler/RestExceptionHandler.java
+++ src/main/java/by/aurorasoft/nominatim/controller/exceptionhandler/RestExceptionHandler.java
@@ -3,10 +3,10 @@ package by.aurorasoft.nominatim.controller.exceptionhandler;
 import by.aurorasoft.nominatim.controller.exception.CustomValidationException;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Value;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.core.convert.ConversionFailedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -23,48 +23,54 @@ import static org.springframework.http.HttpStatus.NOT_ACCEPTABLE;
 
 @ControllerAdvice
 public final class RestExceptionHandler {
-    private static final String MESSAGE_TEMPLATE_NOT_VALID_ENUM_PARAM = "%s should be replaced by one of: %s";
-    private static final String DELIMITER_ENUM_PARAM_ALLOWABLE_VALUE = ", ";
+    private static final String MESSAGE_TEMPLATE_ENUM_CONVERSION_FAILED = "%s should be replaced by one of: %s";
+    private static final String DELIMITER_ENUM_VALUES = ", ";
     private static final String NOT_VALID_ARGUMENT_MESSAGE_DELIMITER = "; ";
+    private static final String FIELD_MESSAGE_DELIMITER = " : ";
 
     @ExceptionHandler
-    public ResponseEntity<RestErrorResponse> handleException(final ConstraintViolationException exception) {
-        return createNotAcceptableResponse(exception);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<RestErrorResponse> handleException(final MethodArgumentNotValidException exception) {
-        return createNotAcceptableResponse(getMessage(exception));
+    public ResponseEntity<RestErrorResponse> handle(final ConstraintViolationException exception) {
+        return createNotAcceptableEntity(exception);
     }
 
     @ExceptionHandler
-    public ResponseEntity<RestErrorResponse> handleException(final CustomValidationException exception) {
-        return createNotAcceptableResponse(exception);
+    public ResponseEntity<RestErrorResponse> handle(final MethodArgumentNotValidException exception) {
+        return createNotAcceptableEntity(getMessage(exception));
     }
 
     @ExceptionHandler
-    public ResponseEntity<RestErrorResponse> handleException(final ConversionFailedException exception) {
-        return createNotAcceptableResponse(getMessage(exception));
+    public ResponseEntity<RestErrorResponse> handle(final CustomValidationException exception) {
+        return createNotAcceptableEntity(exception);
     }
 
-    private static ResponseEntity<RestErrorResponse> createNotAcceptableResponse(final Exception exception) {
-        return createNotAcceptableResponse(exception.getMessage());
+    @ExceptionHandler
+    public ResponseEntity<RestErrorResponse> handle(final ConversionFailedException exception) {
+        return createNotAcceptableEntity(getMessage(exception));
     }
 
-    private static ResponseEntity<RestErrorResponse> createNotAcceptableResponse(final String message) {
-        final RestErrorResponse errorResponse = new RestErrorResponse(NOT_ACCEPTABLE, message, now());
-        return new ResponseEntity<>(errorResponse, NOT_ACCEPTABLE);
+    private static ResponseEntity<RestErrorResponse> createNotAcceptableEntity(final Exception exception) {
+        return createNotAcceptableEntity(exception.getMessage());
+    }
+
+    private static ResponseEntity<RestErrorResponse> createNotAcceptableEntity(final String message) {
+        final HttpStatus status = NOT_ACCEPTABLE;
+        final RestErrorResponse response = new RestErrorResponse(status, message, now());
+        return new ResponseEntity<>(response, status);
     }
 
     private static String getMessage(final MethodArgumentNotValidException exception) {
-        return exception.getAllErrors()
+        return exception.getFieldErrors()
                 .stream()
-                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .map(RestExceptionHandler::getMessage)
                 .collect(joining(NOT_VALID_ARGUMENT_MESSAGE_DELIMITER));
     }
 
+    private static String getMessage(final FieldError error) {
+        return error.getField() + FIELD_MESSAGE_DELIMITER + error.getDefaultMessage();
+    }
+
     private static String getMessage(final ConversionFailedException exception) {
-        return isEnumTarget(exception) ? findEnumParamConversionFailedMessage(exception) : exception.getMessage();
+        return isEnumTarget(exception) ? findEnumConversionFailedMessage(exception) : exception.getMessage();
     }
 
     private static boolean isEnumTarget(final ConversionFailedException exception) {
@@ -72,24 +78,24 @@ public final class RestExceptionHandler {
     }
 
     @SuppressWarnings("unchecked")
-    private static String findEnumParamConversionFailedMessage(final ConversionFailedException exception) {
+    private static String findEnumConversionFailedMessage(final ConversionFailedException exception) {
         final Class<? extends Enum<?>> type = (Class<? extends Enum<?>>) exception.getTargetType().getType();
         return format(
-                MESSAGE_TEMPLATE_NOT_VALID_ENUM_PARAM,
+                MESSAGE_TEMPLATE_ENUM_CONVERSION_FAILED,
                 exception.getValue(),
-                findSeparatedAllowableEnumValues(type)
+                findAllowableEnumValues(type)
         );
     }
 
-    private static String findSeparatedAllowableEnumValues(final Class<? extends Enum<?>> type) {
+    private static String findAllowableEnumValues(final Class<? extends Enum<?>> type) {
         return stream(type.getEnumConstants())
                 .map(Enum::name)
-                .collect(joining(DELIMITER_ENUM_PARAM_ALLOWABLE_VALUE));
+                .collect(joining(DELIMITER_ENUM_VALUES));
     }
 
     @Value
-    private static class RestErrorResponse {
-        HttpStatus httpStatus;
+    static class RestErrorResponse {
+        HttpStatus status;
         String message;
 
         @JsonFormat(shape = STRING, pattern = "yyyy-MM-dd HH-mm-ss")
