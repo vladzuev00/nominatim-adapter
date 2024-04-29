6159fa12aa78201fe98932a1e398b73d20f36de4
Vlad Zuev <vladzuev.00@mail.ru>
2024-04-29 14:04:31 +0300

    refactor TrackCityGeometryLoaderFromRepository

--- src/main/java/by/aurorasoft/distanceclassifier/service/distanceclassifying/loader/TrackCityGeometryLoaderFromRepository.java
+++ src/main/java/by/aurorasoft/distanceclassifier/service/distanceclassifying/loader/TrackCityGeometryLoaderFromRepository.java
@@ -1,7 +1,9 @@
 package by.aurorasoft.distanceclassifier.service.distanceclassifying.loader;
 
+import by.aurorasoft.distanceclassifier.crud.model.dto.City.CityGeometry;
 import by.aurorasoft.distanceclassifier.crud.service.CityService;
-import by.aurorasoft.distanceclassifier.model.PreparedBoundedGeometry;
+import by.aurorasoft.distanceclassifier.model.PreparedCityGeometry;
+import by.aurorasoft.distanceclassifier.service.distanceclassifying.preparer.CityGeometryPreparer;
 import by.aurorasoft.distanceclassifier.service.distanceclassifying.simplifier.TrackSimplifier;
 import by.aurorasoft.distanceclassifier.service.geometry.GeometryService;
 import org.locationtech.jts.geom.LineString;
@@ -9,22 +11,29 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 import java.util.Set;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toUnmodifiableSet;
 
 @Component
 @ConditionalOnProperty(prefix = "distance-classifying", name = "load-city-geometries-on-start-app", havingValue = "false")
 public final class TrackCityGeometryLoaderFromRepository extends TrackCityGeometryLoader {
     private final CityService cityService;
+    private final CityGeometryPreparer geometryPreparer;
 
     public TrackCityGeometryLoaderFromRepository(final TrackSimplifier trackSimplifier,
                                                  final GeometryService geometryService,
-                                                 final CityService cityService) {
+                                                 final CityService cityService,
+                                                 final CityGeometryPreparer geometryPreparer) {
         super(trackSimplifier, geometryService);
         this.cityService = cityService;
+        this.geometryPreparer = geometryPreparer;
     }
 
     @Override
-    protected Set<PreparedBoundedGeometry> loadInternal(final LineString line) {
-        return null;
-//        return cityService.findBoundedPreparedGeometries(line);
+    protected Set<PreparedCityGeometry> loadInternal(final LineString line) {
+        try (final Stream<CityGeometry> geometries = cityService.findIntersectedGeometries(line)) {
+            return geometries.map(geometryPreparer::prepare).collect(toUnmodifiableSet());
+        }
     }
 }
