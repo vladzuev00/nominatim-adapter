50e52371ad3400a43e9668b786d63180827fca34
Vlad Zuev <vladzuev.00@mail.ru>
2024-04-30 12:38:32 +0300

    add CityGeometryPreparerTest

---
+++ src/test/java/by/aurorasoft/distanceclassifier/service/distanceclassifying/geometrypreparer/CityGeometryPreparerTest.java
@@ -0,0 +1,45 @@
+package by.aurorasoft.distanceclassifier.service.distanceclassifying.geometrypreparer;
+
+import by.aurorasoft.distanceclassifier.crud.model.dto.City.CityGeometry;
+import by.aurorasoft.distanceclassifier.model.PreparedCityGeometry;
+import org.junit.Test;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.prep.PreparedGeometry;
+import org.locationtech.jts.geom.prep.PreparedGeometryFactory;
+import org.mockito.MockedStatic;
+
+import static org.junit.Assert.assertEquals;
+import static org.locationtech.jts.geom.prep.PreparedGeometryFactory.prepare;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+
+public final class CityGeometryPreparerTest {
+    private final CityGeometryPreparer preparer = new CityGeometryPreparer();
+
+    @Test
+    public void geometryShouldBePrepared() {
+        try (final MockedStatic<PreparedGeometryFactory> mockedFactory = mockStatic(PreparedGeometryFactory.class)) {
+            final Geometry givenGeometry = mock(Geometry.class);
+            final Geometry givenBoundingBox = mock(Geometry.class);
+            final CityGeometry givenCityGeometry = new CityGeometry(givenGeometry, givenBoundingBox);
+
+            final PreparedGeometry givenPreparedGeometry = mockPreparedGeometryFor(givenGeometry, mockedFactory);
+            final PreparedGeometry givenPreparedBoundingBox = mockPreparedGeometryFor(givenBoundingBox, mockedFactory);
+
+            final PreparedCityGeometry actual = preparer.prepare(givenCityGeometry);
+            final PreparedCityGeometry expected = new PreparedCityGeometry(
+                    givenPreparedGeometry,
+                    givenPreparedBoundingBox
+            );
+            assertEquals(expected, actual);
+        }
+    }
+
+    private static PreparedGeometry mockPreparedGeometryFor(final Geometry geometry,
+                                                            final MockedStatic<PreparedGeometryFactory> mockedFactory) {
+        final PreparedGeometry preparedGeometry = mock(PreparedGeometry.class);
+        mockedFactory.when(() -> prepare(same(geometry))).thenReturn(preparedGeometry);
+        return preparedGeometry;
+    }
+}
