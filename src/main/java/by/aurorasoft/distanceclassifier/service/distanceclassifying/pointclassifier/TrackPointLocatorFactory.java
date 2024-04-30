54bfda0e095315a057c86ab46d9ede2699749ab0
Vlad Zuev <vladzuev.00@mail.ru>
2024-04-30 10:37:22 +0300

    add TrackPointLocatorFactory

---
+++ src/main/java/by/aurorasoft/distanceclassifier/service/distanceclassifying/pointclassifier/TrackPointLocatorFactory.java
@@ -0,0 +1,22 @@
+package by.aurorasoft.distanceclassifier.service.distanceclassifying.pointclassifier;
+
+import by.aurorasoft.distanceclassifier.model.PreparedCityGeometry;
+import by.aurorasoft.distanceclassifier.model.Track;
+import by.aurorasoft.distanceclassifier.service.distanceclassifying.loader.TrackCityGeometryLoader;
+import by.aurorasoft.distanceclassifier.service.geometry.GeometryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+@RequiredArgsConstructor
+public final class TrackPointLocatorFactory {
+    private final TrackCityGeometryLoader trackCityGeometryLoader;
+    private final GeometryService geometryService;
+
+    public TrackPointLocator create(final Track track, final int citySpeedThreshold) {
+        final Set<PreparedCityGeometry> cityGeometries = trackCityGeometryLoader.load(track);
+        return new TrackPointLocator(geometryService, cityGeometries, citySpeedThreshold);
+    }
+}
