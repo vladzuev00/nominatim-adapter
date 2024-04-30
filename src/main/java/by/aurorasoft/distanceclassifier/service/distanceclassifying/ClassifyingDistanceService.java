066e4b84a15aa6bd91e65376f69f1a12cd45ec47
Vlad Zuev <vladzuev.00@mail.ru>
2024-04-30 11:04:12 +0300

    refactor ClassifyingDistanceService

--- src/main/java/by/aurorasoft/distanceclassifier/service/distanceclassifying/ClassifyingDistanceService.java
+++ src/main/java/by/aurorasoft/distanceclassifier/service/distanceclassifying/ClassifyingDistanceService.java
@@ -1,59 +1,20 @@
 package by.aurorasoft.distanceclassifier.service.distanceclassifying;
 
-import by.aurorasoft.distanceclassifier.model.PreparedCityGeometry;
 import by.aurorasoft.distanceclassifier.model.Track;
-import by.aurorasoft.distanceclassifier.model.TrackPoint;
-import by.aurorasoft.distanceclassifier.service.distanceclassifying.loader.TrackCityGeometryLoader;
-import by.aurorasoft.distanceclassifier.service.geometry.GeometryService;
-import by.nhorushko.classifieddistance.ClassifiedDistance;
+import by.aurorasoft.distanceclassifier.service.distanceclassifying.accumulator.ClassifiedDistanceAccumulator;
+import by.aurorasoft.distanceclassifier.service.distanceclassifying.accumulator.ClassifiedDistanceAccumulatorFactory;
 import by.nhorushko.classifieddistance.ClassifiedDistanceStorage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
-import java.util.concurrent.atomic.LongAccumulator;
-
 @Service
 @RequiredArgsConstructor
 public final class ClassifyingDistanceService {
-    private final TrackCityGeometryLoader trackCityGeometryLoader;
-    private final GeometryService geometryService;
+    private final ClassifiedDistanceAccumulatorFactory distanceAccumulatorFactory;
 
     public ClassifiedDistanceStorage classify(final Track track, final int urbanSpeedThreshold) {
-        final StorageAccumulator distanceAccumulator = createDistanceAccumulator(track, urbanSpeedThreshold);
-        track.getPoints().forEach(distanceAccumulator::accumulate);
-        return distanceAccumulator.createStorage();
-    }
-
-    private StorageAccumulator createDistanceAccumulator(final Track track, final int urbanSpeedThreshold) {
-        final Set<PreparedCityGeometry> cityGeometries = trackCityGeometryLoader.load(track);
-        final PointLocator pointLocator = new PointLocator(geometryService, cityGeometries, urbanSpeedThreshold);
-        return new StorageAccumulator(pointLocator);
-    }
-
-    @RequiredArgsConstructor
-    static final class StorageAccumulator {
-        private final PointLocator pointLocator;
-        private double gpsUrban;
-        private double gpsCountry;
-        private double odometerUrban;
-        private double odometerCountry;
-
-        public void accumulate(final TrackPoint point) {
-            if (pointLocator.isUrban(point)) {
-                gpsUrban += point.getGpsDistance().getRelative();
-                odometerUrban += point.getOdometerDistance().getRelative();
-            } else {
-                gpsCountry += point.getGpsDistance().getRelative();
-                odometerCountry += point.getOdometerDistance().getRelative();
-            }
-        }
-
-        public ClassifiedDistanceStorage createStorage() {
-            return new ClassifiedDistanceStorage(
-                    new ClassifiedDistance(gpsUrban, gpsCountry),
-                    new ClassifiedDistance(odometerUrban, odometerCountry)
-            );
-        }
+        final ClassifiedDistanceAccumulator accumulator = distanceAccumulatorFactory.create(track, urbanSpeedThreshold);
+        track.getPoints().forEach(accumulator::accumulate);
+        return accumulator.get();
     }
 }
