a3ea31477eda023380ec21e1fe94aaffb1e64c52
Vlad Zuev <vladzuev.00@mail.ru>
2024-05-06 12:51:55 +0300

    refactor CityScanningService

--- src/main/java/by/aurorasoft/distanceclassifier/service/cityscan/CityScanningService.java
+++ src/main/java/by/aurorasoft/distanceclassifier/service/cityscan/CityScanningService.java
@@ -1,7 +1,6 @@
 package by.aurorasoft.distanceclassifier.service.cityscan;
 
 import by.aurorasoft.distanceclassifier.crud.model.dto.City;
-import by.aurorasoft.distanceclassifier.crud.model.dto.City.CityGeometry;
 import by.aurorasoft.distanceclassifier.crud.service.CityService;
 import by.aurorasoft.distanceclassifier.model.AreaCoordinate;
 import by.aurorasoft.distanceclassifier.service.cityscan.locationappender.ScannedLocationAppender;
@@ -10,22 +9,20 @@ import by.aurorasoft.distanceclassifier.service.cityscan.overpass.OverpassClient
 import lombok.RequiredArgsConstructor;
 import org.locationtech.jts.geom.Geometry;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Set;
-import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.*;
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
 
 @Service
 @RequiredArgsConstructor
-public class CityScanningService {
+public final class CityScanningService {
     private final OverpassClient overpassClient;
     private final OverpassCityFactory cityFactory;
     private final CityService cityService;
     private final ScannedLocationAppender scannedLocationAppender;
 
-    @Transactional(readOnly = true)
     public void scan(final AreaCoordinate areaCoordinate) {
         saveNotExistingCities(areaCoordinate);
         scannedLocationAppender.append(areaCoordinate);
@@ -33,19 +30,13 @@ public class CityScanningService {
 
     @SuppressWarnings("ResultOfMethodCallIgnored")
     private void saveNotExistingCities(final AreaCoordinate areaCoordinate) {
-        final Set<Geometry> existingCityGeometries = findExistingCityGeometries();
+        final Set<Geometry> existingCityGeometries = cityService.findGeometries();
         overpassClient.findCities(areaCoordinate)
                 .getRelations()
                 .stream()
                 .map(cityFactory::create)
                 .filter(city -> !isContainCityGeometry(existingCityGeometries, city))
                 .collect(collectingAndThen(toList(), cityService::saveAll));
-    }
-
-    private Set<Geometry> findExistingCityGeometries() {
-        try (final Stream<CityGeometry> geometries = cityService.findCityGeometries()) {
-            return geometries.map(CityGeometry::getGeometry).collect(toUnmodifiableSet());
-        }
     }
 
     private boolean isContainCityGeometry(final Set<Geometry> geometries, final City city) {
