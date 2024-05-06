e50577fc8eb42e7e34c6df32aa33c2faf07faafd
Vlad Zuev <vladzuev.00@mail.ru>
2024-05-06 10:40:46 +0300

    add scanned_location sql table

--- src/main/resources/db/changelog/scripts/1.0.sql
+++ src/main/resources/db/changelog/scripts/1.0.sql
@@ -15,3 +15,24 @@ ALTER SEQUENCE city_id_seq INCREMENT 50;
 
 CREATE INDEX city_bounding_box_index ON city USING GIST(bounding_box);
 
+CREATE TABLE scanned_location
+(
+    id INTEGER PRIMARY KEY,
+    geometry GEOMETRY NOT NULL
+);
+
+INSERT INTO scanned_location(id, geometry) VALUES(1, ST_GeomFromText('POLYGON EMPTY', 4326));
+
+CREATE FUNCTION deny_insert_and_delete_scanned_location()
+  RETURNS TRIGGER
+AS
+$body$
+BEGIN
+  RAISE EXCEPTION 'Impossible to insert or delete scanned location';
+END;
+$body$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_on_insert_and_delete_scanned_location
+BEFORE INSERT OR DELETE ON scanned_location
+FOR EACH STATEMENT EXECUTE PROCEDURE deny_insert_and_delete_scanned_location();
