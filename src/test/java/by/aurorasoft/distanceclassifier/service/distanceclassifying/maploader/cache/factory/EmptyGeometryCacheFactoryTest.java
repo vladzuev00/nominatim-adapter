49b7747eb5fb2374cf01be8233e17e7e2b16a0eb
Vlad Zuev <vladzuev.00@mail.ru>
2024-05-07 12:23:23 +0300

    add EmptyGeometryCacheFactoryTest

---
+++ src/test/java/by/aurorasoft/distanceclassifier/service/distanceclassifying/maploader/cache/factory/EmptyGeometryCacheFactoryTest.java
@@ -0,0 +1,49 @@
+package by.aurorasoft.distanceclassifier.service.distanceclassifying.maploader.cache.factory;
+
+import by.aurorasoft.distanceclassifier.crud.model.dto.City.CityGeometry;
+import by.aurorasoft.distanceclassifier.service.geometry.GeometryService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.Polygon;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public final class EmptyGeometryCacheFactoryTest {
+
+    @Mock
+    private GeometryService mockedGeometryService;
+
+    private EmptyGeometryCacheFactory factory;
+
+    @Before
+    public void initializeFactory() {
+        factory = new EmptyGeometryCacheFactory(null, mockedGeometryService);
+    }
+
+    @Test
+    public void cityGeometriesShouldBeGot() {
+        final Stream<CityGeometry> actual = factory.getCityGeometries();
+
+        final boolean actualEmpty = actual.findAny().isEmpty();
+        assertTrue(actualEmpty);
+    }
+
+    @Test
+    public void scannedGeometryShouldBeGot() {
+        final Polygon givenGeometry = mock(Polygon.class);
+        when(mockedGeometryService.createEmptyPolygon()).thenReturn(givenGeometry);
+
+        final Geometry actual = factory.getScannedGeometry();
+        assertSame(givenGeometry, actual);
+    }
+}
