9cb20499314cfc3ed46e9556ea19fd1f1a45106f
Vlad Zuev <vladzuev.00@mail.ru>
2024-05-10 11:01:13 +0300

    refactor TrackCityMapLoadBenchmarkTest and TrackCityMapLoadFromRepositoryBenchmarkTest

--- src/test/java/by/aurorasoft/distanceclassifier/benchmark/cityloading/TrackCityMapLoadBenchmarkTest.java
+++ src/test/java/by/aurorasoft/distanceclassifier/benchmark/cityloading/TrackCityMapLoadBenchmarkTest.java
@@ -5,9 +5,12 @@ import by.aurorasoft.distanceclassifier.benchmark.base.BenchmarkTest;
 import by.aurorasoft.distanceclassifier.model.Track;
 import by.aurorasoft.distanceclassifier.service.distanceclassifying.maploader.TrackCityMapLoader;
 import org.openjdk.jmh.annotations.Benchmark;
+import org.springframework.test.annotation.DirtiesContext;
 
 import static by.aurorasoft.distanceclassifier.testutil.TrackCSVFileReadUtil.read;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD;
 
+@DirtiesContext(classMode = AFTER_EACH_TEST_METHOD)
 public abstract class TrackCityMapLoadBenchmarkTest extends BenchmarkTest {
     private static final String TRACK_FILE_NAME = "track-6.csv";
     private static final Track TRACK = read(TRACK_FILE_NAME);
--- src/test/java/by/aurorasoft/distanceclassifier/benchmark/cityloading/TrackCityMapLoadFromRepositoryBenchmarkTest.java
+++ src/test/java/by/aurorasoft/distanceclassifier/benchmark/cityloading/TrackCityMapLoadFromRepositoryBenchmarkTest.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest(
         properties = {
                 "distance-classifying.cache-geometries=false",
+                "spring.jpa.properties.hibernate.show_sql=false",
                 "spring.jpa.properties.hibernate.hbm2ddl.import_files=classpath:sql/insert-belarus-cities.sql"
         }
 )
