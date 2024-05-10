71281e760ef0a481568e36d1dbc01a706577693c
Vlad Zuev <vladzuev.00@mail.ru>
2024-05-10 17:53:52 +0300

    add ClassifyingDistanceProperty

--- src/main/java/by/aurorasoft/distanceclassifier/ApplicationRunner.java
+++ src/main/java/by/aurorasoft/distanceclassifier/ApplicationRunner.java
@@ -1,10 +1,12 @@
 package by.aurorasoft.distanceclassifier;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class ApplicationRunner {
     public static void main(final String[] args) {
         run(ApplicationRunner.class, args);
---
+++ src/main/java/by/aurorasoft/distanceclassifier/config/property/ClassifyingDistanceProperty.java
@@ -0,0 +1,29 @@
+package by.aurorasoft.distanceclassifier.config.property;
+
+import lombok.*;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+
+@RequiredArgsConstructor
+@Getter
+@EqualsAndHashCode
+@ToString
+@Builder
+@ConstructorBinding
+@ConfigurationProperties("distance-classifying")
+public final class ClassifyingDistanceProperty {
+
+    @NotNull
+    private final Boolean cacheGeometries;
+
+    @NotNull
+    @DecimalMin("0")
+    private final Double trackSimplifyEpsilon;
+
+    @NotNull
+    @DecimalMin("0")
+    private final Double significantPointGpsAbsoluteThreshold;
+}
