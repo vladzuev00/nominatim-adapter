f3f1b65e0a1e9b7d7fa26f5f85643897ace65d18
Vlad Zuev <vladzuev.00@mail.ru>
2024-05-16 16:36:20 +0300

    refactor

--- src/test/java/by/aurorasoft/distanceclassifier/benchmark/classifyingdistance/ClassifyDistanceWithCachedGeometriesBenchmarkTest.java
+++ src/test/java/by/aurorasoft/distanceclassifier/benchmark/classifyingdistance/ClassifyDistanceWithCachedGeometriesBenchmarkTest.java
@@ -4,11 +4,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(
         properties = {
-                "distance-classifying.cache-geometries=false",
-                "spring.jpa.properties.hibernate.show_sql=false",
+                "distance-classifying.cache-geometries=true",
                 "spring.jpa.properties.hibernate.hbm2ddl.import_files=classpath:sql/insert-belarus-cities.sql"
         }
 )
-public class ClassifyDistanceLoadingCityMapFromRepositoryBenchmarkTest extends ClassifyDistanceBenchmarkTest {
+public class ClassifyDistanceWithCachedGeometriesBenchmarkTest extends ClassifyDistanceBenchmarkTest {
 
 }
--- src/test/java/by/aurorasoft/distanceclassifier/benchmark/classifyingdistance/ClassifyDistanceWithNotCachedGeometriesBenchmarkTest.java
+++ src/test/java/by/aurorasoft/distanceclassifier/benchmark/classifyingdistance/ClassifyDistanceWithNotCachedGeometriesBenchmarkTest.java
@@ -4,10 +4,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(
         properties = {
-                "distance-classifying.cache-geometries=true",
+                "distance-classifying.cache-geometries=false",
+                "spring.jpa.properties.hibernate.show_sql=false",
                 "spring.jpa.properties.hibernate.hbm2ddl.import_files=classpath:sql/insert-belarus-cities.sql"
         }
 )
-public class ClassifyDistanceLoadingCityMapFromCacheBenchmarkTest extends ClassifyDistanceBenchmarkTest {
+public class ClassifyDistanceWithNotCachedGeometriesBenchmarkTest extends ClassifyDistanceBenchmarkTest {
 
 }
