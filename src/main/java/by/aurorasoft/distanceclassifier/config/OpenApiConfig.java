7de35b0d202d390ed6fb628cf8595cf2804d427d
Nikolay <nikolay.horushko@gmail.com>
2024-05-27 16:58:45 +0300

    Refactor pom.xml and OpenApiConfig.java to update dependencies and streamline OpenAPI configuration

--- src/main/java/by/aurorasoft/distanceclassifier/config/OpenApiConfig.java
+++ src/main/java/by/aurorasoft/distanceclassifier/config/OpenApiConfig.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springdoc.core.utils.SpringDocUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,20 +16,16 @@ import java.time.format.DateTimeFormatter;
 @Configuration
 public class OpenApiConfig {
 
-    @Value("${app.version}")
-    private String appVersion;
 
     @Bean
     public OpenAPI openApi(@Value("${server.servlet.context-path}") String baseUrl) {
 
         var schema = new Schema<LocalDateTime>();
         schema.example(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)).type("string");
-        SpringDocUtils.getConfig().replaceWithSchema(LocalDateTime.class, schema);
 
         return new OpenAPI()
                 .info(new Info()
-                        .title("Aurora API mileage classificator")
-                        .version(appVersion))
+                        .title("Aurora API mileage classificator"))
                 .addSecurityItem(new SecurityRequirement()
                         .addList("Api-Key-authorization"))
                 .components(new Components()
