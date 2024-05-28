b3726df42f778aa3dcf44fdc392b381e2ea9e11b
Nikolay <nikolay.horushko@gmail.com>
2024-05-28 16:56:30 +0300

    Add logging to ClassifyDistanceController for performance metrics and detailed classification results

--- src/main/java/by/aurorasoft/distanceclassifier/controller/classifydistance/ClassifyDistanceController.java
+++ src/main/java/by/aurorasoft/distanceclassifier/controller/classifydistance/ClassifyDistanceController.java
@@ -6,6 +6,7 @@ import by.aurorasoft.distanceclassifier.model.Track;
 import by.aurorasoft.distanceclassifier.service.distanceclassify.ClassifyDistanceService;
 import by.nhorushko.classifieddistance.ClassifiedDistanceStorage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import javax.validation.Valid;
 
 import static org.springframework.http.ResponseEntity.ok;
 
+@Slf4j
 @Validated
 @RestController
 @RequestMapping("/api/v1/classifyDistance")
@@ -27,8 +29,15 @@ public class ClassifyDistanceController {
 
     @PostMapping
     public ResponseEntity<ClassifiedDistanceStorage> classify(@Valid @RequestBody final ClassifyDistanceRequest request) {
+        long start = System.currentTimeMillis();
         final Track track = trackFactory.create(request);
         final ClassifiedDistanceStorage storage = service.classify(track, request.getUrbanSpeedThreshold());
+        log.info("Classify distance: {} km, urban: {} km, country: {} km, time {} ms",
+                storage.getGpsDistance().getTotal(),
+                storage.getGpsDistance().getUrban(),
+                storage.getGpsDistance().getCountry(),
+                System.currentTimeMillis() - start
+        );
         return ok(storage);
     }
 }
