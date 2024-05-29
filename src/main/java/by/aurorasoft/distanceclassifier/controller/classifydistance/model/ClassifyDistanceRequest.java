1548f116a1220f1a9ae4b228ad60c77f2d73f49c
Nikolay <nikolay.horushko@gmail.com>
2024-05-29 12:37:40 +0300

    Refactor ClassifyDistanceRequest to use primitive types for numeric fields and remove redundant null checks

--- src/main/java/by/aurorasoft/distanceclassifier/controller/classifydistance/model/ClassifyDistanceRequest.java
+++ src/main/java/by/aurorasoft/distanceclassifier/controller/classifydistance/model/ClassifyDistanceRequest.java
@@ -22,12 +22,12 @@ public class ClassifyDistanceRequest {
 
     @NotNull
     @PositiveOrZero
-    Integer urbanSpeedThreshold;
+    int urbanSpeedThreshold;
 
     @Builder
     @JsonCreator
     public ClassifyDistanceRequest(@JsonProperty("trackPoints") final List<PointRequest> trackPoints,
-                                   @JsonProperty("urbanSpeedThreshold") final Integer urbanSpeedThreshold) {
+                                   @JsonProperty("urbanSpeedThreshold") int urbanSpeedThreshold) {
         this.trackPoints = trackPoints;
         this.urbanSpeedThreshold = urbanSpeedThreshold;
     }
@@ -36,14 +36,13 @@ public class ClassifyDistanceRequest {
     public static class PointRequest {
 
         @Latitude
-        Double latitude;
+        double latitude;
 
         @Longitude
-        Double longitude;
+        double longitude;
 
-        @NotNull
         @PositiveOrZero
-        Integer speed;
+        int speed;
 
         @NotNull
         DistanceRequest gpsDistance;
@@ -53,11 +52,11 @@ public class ClassifyDistanceRequest {
 
         @Builder
         @JsonCreator
-        public PointRequest(@JsonProperty("latitude") final Double latitude,
-                            @JsonProperty("longitude") final Double longitude,
-                            @JsonProperty("speed") final Integer speed,
-                            @JsonProperty("gpsDistance") final DistanceRequest gpsDistance,
-                            @JsonProperty("odometerDistance") final DistanceRequest odometerDistance) {
+        public PointRequest(@JsonProperty("latitude") double latitude,
+                            @JsonProperty("longitude") double longitude,
+                            @JsonProperty("speed") int speed,
+                            @JsonProperty("gpsDistance") DistanceRequest gpsDistance,
+                            @JsonProperty("odoDistance") DistanceRequest odometerDistance) {
             this.latitude = latitude;
             this.longitude = longitude;
             this.speed = speed;
@@ -69,18 +68,16 @@ public class ClassifyDistanceRequest {
     @Value
     public static class DistanceRequest {
 
-        @NotNull
         @PositiveOrZero
-        Double relative;
+        double relative;
 
-        @NotNull
         @PositiveOrZero
-        Double absolute;
+        double absolute;
 
         @Builder
         @JsonCreator
-        public DistanceRequest(@JsonProperty("relative") final Double relative,
-                               @JsonProperty("absolute") final Double absolute) {
+        public DistanceRequest(@JsonProperty("relative") double relative,
+                               @JsonProperty("absolute") double absolute) {
             this.relative = relative;
             this.absolute = absolute;
         }
