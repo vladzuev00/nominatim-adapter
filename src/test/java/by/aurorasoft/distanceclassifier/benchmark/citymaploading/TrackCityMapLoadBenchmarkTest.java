1d29a9acd81ca3418a50563034331c853544cbef
Vlad Zuev <vladzuev.00@mail.ru>
2024-05-31 10:38:51 +0300

    correct

--- src/test/java/by/aurorasoft/distanceclassifier/benchmark/citymaploading/TrackCityMapLoadBenchmarkTest.java
+++ src/test/java/by/aurorasoft/distanceclassifier/benchmark/citymaploading/TrackCityMapLoadBenchmarkTest.java
@@ -1,13 +1,11 @@
 package by.aurorasoft.distanceclassifier.benchmark.citymaploading;
 
-
 import by.aurorasoft.distanceclassifier.benchmark.base.BenchmarkTest;
 import by.aurorasoft.distanceclassifier.model.Track;
 import by.aurorasoft.distanceclassifier.service.distanceclassify.maploader.TrackCityMapLoader;
 import by.aurorasoft.distanceclassifier.testutil.TrackCSVFileReadUtil;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.springframework.test.annotation.DirtiesContext;
-
 
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public abstract class TrackCityMapLoadBenchmarkTest extends BenchmarkTest {
